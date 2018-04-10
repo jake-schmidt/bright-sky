aabd28eb33e2608f29518e89832edadc42befdc7
Jeff Cohen <cohen.jeff@gmail.com>
2018-04-10 16:06:58 -0500

    Ready

---
+++ 1_weather.rb
@@ -0,0 +1,18 @@
+require 'open-uri'
+require 'json'
+
+# Get raw JSON text from the OpenWeatherMap API
+api_key = '4ce6f502d38ddae567bf1702b05e168c'
+url = "https://api.openweathermap.org/data/2.5/weather"
+url = url + "?q=Chicago&appid=#{api_key}"
+weather_json = open(url).read
+
+# Parse the JSON into a Ruby data structure
+data = JSON.parse(weather_json)
+
+# TO DO: Change this code to fetch the temperature
+# from the `data` variable.
+temperature = 0
+
+# Do not touch the code below this line.
+print "It is #{temperature} degrees outside. \n"
---
+++ 2_divvy.rb
@@ -0,0 +1,21 @@
+require 'open-uri'
+require 'json'
+
+# Get raw JSON text from the Divvy API
+url = "http://bikes.kiei925.com/stations/search.json"
+url = url + "?address=Global+Hub,Northwestern"
+station_json = open(url).read
+
+# Parse the JSON into a Ruby data structure
+data = JSON.parse(station_json)
+
+# TO DO: Change this code to store the correct, live data
+# into the appropriate variables.
+station_name = "Buckingham Fountain"
+bike_count = 5
+dock_count = 3
+
+# Do not touch the code below this line.
+print "The nearest station is #{station_name}.\n"
+print "If you need to rent a bike, there are #{bike_count} bikes available right now.\n"
+print "If you need to return a bike, there are #{dock_count} open docking spaces right now.\n"
